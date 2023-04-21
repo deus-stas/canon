a22d233da5b8f1446fc6fcc93f040bde4e0e74a4
stanislavmuhin <stnslvmukhin@gmail.com>
2023-04-21 15:50:37 +0300

    изменил сортировку

--- f/src/components/Page/Landing/index.jsx
+++ f/src/components/Page/Landing/index.jsx
@@ -1,44 +1,78 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchPage } from '@store'
 import { inFinalState, inInitialState } from '@store/helpers'
 import { useTemplateContext } from '@contexts/TemplateContext'
-import { usePageMeta } from '@hooks'
+import { getCurrentRegion } from '@/components/Header/Regions'
+import { fetchLandings } from '@/store'
 
-import './style.scss'
+// import './style.scss'
 import NotFoundPage from '@components/Page/NotFoundPage'
-
-const pageCode = 'landing'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
-    usePageMeta({ pageCode })
     const lang = useTemplateContext().lang
+    const region = getCurrentRegion()
     const dispatch = useDispatch()
-    const pageStoreChunk = useSelector(store => store['page'][pageCode]);
+    const sectionsStoreChunk = useSelector(store => store['landings'])
 
     useEffect(() => {
-        if (!pageStoreChunk || inInitialState(pageStoreChunk)) {
-            dispatch(fetchPage(pageCode, lang))
+        if (!sectionsStoreChunk || inInitialState(sectionsStoreChunk)) {
+            dispatch(fetchLandings(region, lang))
         }
-        document.documentElement.scrollTop = 0
-    }, [dispatch, pageStoreChunk])
+    }, [dispatch, sectionsStoreChunk])
 
-    if (!inFinalState(pageStoreChunk)) {
+    if (!inFinalState(sectionsStoreChunk)) {
         return null
     }
 
-    const { data: pageData } = pageStoreChunk
+    const { data } = sectionsStoreChunk;
 
-    console.log(pageData);
+    console.log(data);
 
-    return pageData ? (
-        <div>
-            <div className="container">
-                <h1>Имя: {pageData.code}</h1>
-                <h2>Дата: {pageData.date}</h2>
-                <h3>Id: {pageData.id}</h3>
+    return data ? (
+        <div id="primary-content">
+
+            <div id="content-area">
+
+                <div className="featured-section featured-section-noBG fsEH">
+                    <div className="b-container">
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <h2 property="s:title">UPCOMING WEBINARS</h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            {
+                                data.items.map((item) => {
+                                    return (
+                                        <div className="col-sm-6 position-relative" key={item.id}>
+                                            <div className="well well-link" key={item.id}>
+                                                <Link className='extended-modal-image' to={`/landings/${item.code}`}>
+                                                    <span property="s:largeImage">
+                                                        <img src={item.start_image.src} className="img-fluid center-block" alt="" style={{ maxHeight: '176.719px' }} />
+                                                    </span>
+                                                    <h4>
+                                                        <div style={{ color: 'rgb(0, 0, 0)' }}>
+                                                            <p dangerouslySetInnerHTML={{ __html: item.start_text }}></p>
+                                                            <p className="btn btn-default center-block">
+                                                                <i className="zmdi zmdi-plus-circle-o"></i>
+                                                                More Information
+                                                            </p>
+                                                        </div>
+                                                    </h4>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+
+
+        </div >
     ) : <NotFoundPage />
 }
 
