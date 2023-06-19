ed7470cb1ec2c4ed68a04add3f3b5b78016d9d59
stanislavmuhin <stnslvmukhin@gmail.com>
2023-06-19 11:45:51 +0300

    updates

--- local/modules/als.project/lib/Forms.php
+++ local/modules/als.project/lib/Forms.php
@@ -26,7 +26,6 @@ class Forms
 
                 $data           = [];
                 $data['VALUES'] = CFormResult::GetDataByID($resultId, [], $result, $answers);
-
                 $baseUrl      = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/';
                 $data['PATH'] = $baseUrl . 'bitrix/admin/form_result_edit.php?lang=ru&WEB_FORM_ID=' . $params['form_id'] . '&RESULT_ID=' . $resultId;
 
@@ -57,6 +56,55 @@ class Forms
             'message' => 'Module "form" not included',
         ];
     }
+    /**
+     * @throws LoaderException
+     */
+    public static function saveFormPostWarranty(array $params): array
+    {
+        if (Loader::IncludeModule("form")) {
+	        file_put_contents('/www/wwwroot/rpcanon.de-us.ru/sandbox/test.txt',json_encode($params['values'],JSON_UNESCAPED_UNICODE));
+            if ($resultId = CFormResult::Add($params['form_id'], $params['values'])) {
+                $result  = [];
+                $answers = [];
+
+                $data           = [];
+                $data['VALUES'] = CFormResult::GetDataByID($resultId, [], $result, $answers);
+                $baseUrl      = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/';
+                $data['PATH'] = $baseUrl . 'bitrix/admin/form_result_edit.php?lang=ru&WEB_FORM_ID=' . $params['form_id'] . '&RESULT_ID=' . $resultId;
+
+                $template = Mail::getInteractiveTemplate(
+                    'post_warranty/add',
+                    $data
+                );
+                $res_files = [];
+                if($data['VALUES']['files']){
+                    foreach ($data['VALUES']['files'] as $file){
+                        $res_files[] = $file['USER_FILE_ID'];
+                    }
+                }
+                Mail::send([
+                    'TEXT'    => $template,
+                    'EMAIL_TO' => \COption::GetOptionString( "askaron.settings", "UF_EMAIL_TO_POST_WARRANTY"),
+                    'SUBJECT' => 'Canon MS. Постгарантийный сервис. Новый отклик',
+                ],$res_files);
+                Mail::forceSending();
+
+
+                return [
+                    'status' => 'ok',
+                ];
+            }
+
+            return [
+                'status' => 'error',
+            ];
+        }
+
+        return [
+            'status'  => 'error',
+            'message' => 'Module "form" not included',
+        ];
+    }
 
     /**
      * @throws LoaderException
@@ -64,6 +112,14 @@ class Forms
     public static function getFeedback(): array
     {
         return self::getForm(LANGUAGE_CODE === 'en' ? 'feedback_en' : 'feedback');
+    }
+
+    /**
+     * @throws LoaderException
+     */
+    public static function getPostWarranty(): array
+    {
+        return self::getForm(LANGUAGE_CODE === 'en' ? 'post_warranty_en' : 'post_warranty');
     }
 
     /**
@@ -106,6 +162,21 @@ class Forms
                 'AUTOCOMPLETE' => $question["COMMENTS"],
             ];
         }
+        // Постгарантийный сервис
+        $id_related = []; // Сбор id ответов от поля
+        if($sid == 'post_warranty' || $sid == 'post_warranty_en'){
+            foreach ($questions as $key => $question) {
+                if($key == 'related'){
+                    foreach ($answers as $subKey => $answer) {
+                        if($subKey == $key){
+                            foreach ($answer as $value) {
+                                $id_related[] = $value['ID'];
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         //имена полей формируются https://dev.1c-bitrix.ru/api_help/form/htmlnames.php
         $htmlNameSid   = ['radio', 'dropdown', 'checkbox', 'multiselect'];
@@ -122,6 +193,29 @@ class Forms
                     'SORT'       => $value['C_SORT'],
                     'PARENT_ID'  => $value['FIELD_PARAM'],
                 ];
+
+                // Для зависимых полей, связку производим через свойства "параметры" в ответе у поля, прописав туда id ответа от другого поля
+                if($value['FIELD_PARAM']){
+                    $params = explode(",", $value['FIELD_PARAM']); // разбиваем строку на массив
+                    $found = false;
+                    foreach ($params as $param) {
+                        if (in_array($param, $id_related)) {
+                            $found = true;
+                            break;
+                        }
+                    }
+
+                    if ($found) {
+                        // Добавляем в поле, а именно у related (Тема запроса) группе под полей
+                        $arResult['QUESTIONS']['SUB_QUESTIONS']['SID'] = 'SUB_QUESTIONS';
+                        $arResult['QUESTIONS']['SUB_QUESTIONS']['SORT'] = '910';
+                        $arResult['QUESTIONS']['SUB_QUESTIONS']['fields'][] = $arResult['QUESTIONS'][$key];
+
+                        // Убираем ненужные поля из общего списка
+                        unset($arResult['QUESTIONS'][$key]); // очищаем
+                    }
+                }
+
             }
         }
 
